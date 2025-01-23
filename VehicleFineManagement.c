d1980012266b1181e883cf670d0db7db548e13e4
kamillamamatova <kamillamamatova.km@gmail.com>
2025-01-23 01:41:08 -0500

    Update VehicleFineManagement.c

--- VehicleFineManagement.c
+++ VehicleFineManagement.c
@@ -1,5 +1,8 @@
-/* COP 3502C Assignment 5
-This program is written by: Kamilla Mamatova */
+/* This code manages vehicle owners and their fines. 
+The system keeps track of owner names, their fines, 
+and their position in the tree. It provides functionalities 
+for adding, searching, deducting, deleting, calculating, 
+and balancing nodes (representing owners).*/
 
 #include <stdio.h>
 #include <string.h>
