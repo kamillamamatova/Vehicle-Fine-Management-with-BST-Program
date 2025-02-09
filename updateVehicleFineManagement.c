d5460a48d894843fdffd6b89c2d263ff848acd47
kamillamamatova <kamillamamatova.km@gmail.com>
2025-02-09 00:44:04 -0500

    Update updateVehicleFineManagement.c

--- updateVehicleFineManagement.c
+++ updateVehicleFineManagement.c
@@ -1,3 +1,9 @@
+/* This code manages vehicle owners and their fines. 
+The system keeps track of owner names, their fines, 
+and their position in the tree. It provides functionalities 
+for adding, searching, deducting, deleting, calculating, 
+and balancing nodes (representing owners).*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
